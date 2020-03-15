b0116eef1187136fffc9b5843b1c1a9f8483cae1
Ahmed Essam <eng.eramax@gmail.com>
2020-03-15 18:19:30 +0100

    init

---
+++ MVC_K8S/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MVC_K8S.Models;
+
+namespace MVC_K8S.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
---
+++ MVC_K8S/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace MVC_K8S.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Count { get; set; }
+    }
+}
